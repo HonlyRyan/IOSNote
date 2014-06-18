15e7bef484d9d983ccc0e82c08867ec35926b0a7
WangHao <bingoogol@sina.com>
2014-06-18 22:30:42 +0800

    2048绘制背景

--- Swift/swift2048/swift2048/ViewController.swift
+++ Swift/swift2048/swift2048/ViewController.swift
@@ -19,7 +19,18 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func startGame(sender:UIButton) {
+        let alertView = UIAlertView()
+        alertView.title = "开始"
+        alertView.message = "游戏就要开始，你准备好了吗？"
+        alertView.addButtonWithTitle("Ready Go!")
+        alertView.show()
+        alertView.delegate = self
+    }
 
-
+    func alertView(alertView:UIAlertView,clickedButtonAtIndex buttonIndex:Int) {
+        self.presentViewController(MainTabViewController(),animated:true,completion:nil)
+    }
 }
 
